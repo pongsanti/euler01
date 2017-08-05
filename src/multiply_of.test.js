1c191015a5be76e09430f64419a5bc9051304032
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-08-05 23:30:18 +0700

    add tests

--- src/multiply_of.test.js
+++ src/multiply_of.test.js
@@ -4,6 +4,14 @@ test('6 is multiplied of 3', () => {
   expect(mulOf3(6)).toBeTruthy()
 })
 
+test('6 is not multiplied of 2', () => {
+  expect(mulOf3(2)).toBeFalsy()
+})
+
 test('10 is multiplied of 5', () => {
   expect(mulOf5(10)).toBeTruthy()
+})
+
+test('4 is not multiplied of 5', () => {
+  expect(mulOf5(4)).toBeFalsy()
 })
